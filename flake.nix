13bbbbeecd99615e069c687482770a0203f2a1d4
Aqeel Akber <aqeel@aqeelakber.com>
2024-11-19 19:05:11 +1100

    fix(flake): Attempting to use mkApp for wrapping

--- flake.nix
+++ flake.nix
@@ -1,16 +1,13 @@
 {
   description = "nix-extract-flake-programs - A script to document and compare packages in Nix configurations.";
 
-  outputs = { self }: {
+  outputs = { self, ... }: {
     # Define the default package
     defaultPackage.x86_64-linux = import ./default.nix;
 
-    # Define the package
-    packages.x86_64-linux.default = self.defaultPackage.x86_64-linux;
-
-    # Define an app for direct execution
-    apps.default = {
-      type = "app";
+    # Define the package as an app
+    packages.x86_64-linux.default = pkgs: pkgs.stdenv.mkApp {
+      drv = self.defaultPackage.x86_64-linux;
       program = "${self.defaultPackage.x86_64-linux}/bin/nix-extract-flake-programs";
       description = "Extract and format package lists from Nix configurations for documentation or comparison.";
     };
